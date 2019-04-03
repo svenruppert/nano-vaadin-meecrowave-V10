680d9c306dd75514e1819ee8b4454146a78ab15e
Sven Ruppert <sven.ruppert@gmail.com>
2019-04-03 10:48:31 +0200

    update versions, switched to code level 8

--- src/main/java/org/rapidpm/vaadin/v10/tb/demo/VaadinApp.java
+++ src/main/java/org/rapidpm/vaadin/v10/tb/demo/VaadinApp.java
@@ -25,15 +25,15 @@ import com.vaadin.flow.router.Route;
 import org.rapidpm.dependencies.core.logger.HasLogger;
 
 import static java.lang.String.valueOf;
-import static org.rapidpm.vaadin.addons.framework.ComponentIDGenerator.buttonID;
-import static org.rapidpm.vaadin.addons.framework.ComponentIDGenerator.spanID;
+//import static org.rapidpm.vaadin.addons.framework.ComponentIDGenerator.buttonID;
+//import static org.rapidpm.vaadin.addons.framework.ComponentIDGenerator.spanID;
 
 @Route("")
 public class VaadinApp extends Composite<Div> implements HasLogger {
 
   // read http://vaadin.com/testing for more infos
-  public static final String BTN_CLICK_ME   = buttonID().apply(VaadinApp.class, "btn-click-me");
-  public static final String LB_CLICK_COUNT = spanID().apply(VaadinApp.class, "lb-click-count");
+//  public static final String BTN_CLICK_ME   = buttonID().apply(VaadinApp.class, "btn-click-me");
+//  public static final String LB_CLICK_COUNT = spanID().apply(VaadinApp.class, "lb-click-count");
 
   private final Button         btnClickMe   = new Button("click me");
   private final Span           lbClickCount = new Span("0");
@@ -42,10 +42,10 @@ public class VaadinApp extends Composite<Div> implements HasLogger {
   private int clickcount = 0;
 
   public VaadinApp() {
-    btnClickMe.setId(BTN_CLICK_ME);
+//    btnClickMe.setId(BTN_CLICK_ME);
     btnClickMe.addClickListener(event -> lbClickCount.setText(valueOf(++clickcount)));
 
-    lbClickCount.setId(LB_CLICK_COUNT);
+//    lbClickCount.setId(LB_CLICK_COUNT);
 
     //set the main Component
     logger().info("setting now the main ui content..");
